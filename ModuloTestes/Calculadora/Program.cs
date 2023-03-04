4900f93f378160514e1cf7798883589e8862b280
Alerrandro Janio <alerrandro.janio07@gmail.com>
2023-03-03 22:49:09 -0300

    modulo de testes unitarios

---
+++ ModuloTestes/Calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿int numeroDeNotas = 0;
+double[] nota = new double[4];
+int i = 0;
+
+while (i < 4)
+{
+    nota[i] = Double.Parse(Console.ReadLine());
+
+    if (nota[i] < 0 || nota[i] > 11)
+    {
+        numeroDeNotas++;
+        Console.WriteLine("nota invalida");
+    }
+
+    if (numeroDeNotas == 2)
+    {
+        break;
+    }
+
+    i++;
+}
+
+double media = (nota[1] + nota[1] + nota[1] + nota[1]) / 4;
+
+Console.WriteLine($"media = {media}");
