f79e3754043cb97e459341457dfb89793f72f9a5
Alerrandro Janio <alerrandro.janio07@gmail.com>
2023-03-06 15:24:55 -0300

    add .gitignore

---
+++ AzureBlobStorage/Controllers/ArquivosController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobStorage.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArquivosController : ControllerBase { }
+}
